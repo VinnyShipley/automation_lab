712c75a7aaea8aba1da07fe7dd65516f3e977bbc
Vinny <vinnyshipley@gmail.com>
2022-08-11 21:00:27 -0700

    8/11

--- automation/automation.py
+++ automation/automation.py
@@ -3,14 +3,12 @@ import re
 with open('../assets/potential-contacts.txt') as f:
   text_from_file = f.read()
 
-
-email_pattern = 8
-
 phone_pattern = r"(\d{3}.\d{3}.\d{4})"
 
+# finding phone number digits
 phone_nums = re.findall(phone_pattern, text_from_file)
 
-
+#list where the phone number digits will live
 phone_numbers_raw = []
 
 
@@ -29,20 +27,19 @@ for num in phone_nums:
     joined_nums = singular_phone_number.join(number)
     singular_phone_number += number
     
-    # adds the non numerical 
+    # adds the non numerical characters to phone number for structuring
     if len(singular_phone_number) == 3:
       singular_phone_number += '-'
-    # if len(singular_phone_number) == 5:
-    #   singular_phone_number += '-'
     if len(singular_phone_number) == 7:
       singular_phone_number += '-'
     if len(singular_phone_number) == 12:
       phone_numbers_raw.append(singular_phone_number)
-print(phone_numbers_raw)
 
+
+#gets rid of repeats
 no_dupe_phone = [*set(phone_numbers_raw)]
 
-
+#writes to the file
 with open('../assets/phone_numbers.txt', 'w') as f:
   for nums in no_dupe_phone:
     f.write(nums + '\n')
