e9eca9007a56a7ff8a412b4d10b35766e6a19294
Vinny <vinnyshipley@gmail.com>
2022-08-11 18:29:14 -0700

    phone numbers each print on own line in text file

--- automation/automation.py
+++ automation/automation.py
@@ -9,14 +9,14 @@ email_pattern = 8
 phone_pattern = r"(\d{3}.\d{3}.\d{4})"
 
 phone_nums = re.findall(phone_pattern, text_from_file)
-# print(phone_nums)
+
 
 phone_numbers_raw = []
 
 
 # Gets phone number in the correct form
 for num in phone_nums:
-  singular_phone_number = '('
+  singular_phone_number = ''
   for number in num:
     if number == ')':
       continue
@@ -28,17 +28,21 @@ for num in phone_nums:
       continue
     joined_nums = singular_phone_number.join(number)
     singular_phone_number += number
-    # print(singular_phone_number)
-    if len(singular_phone_number) == 4:
-      singular_phone_number += ')'
-    if len(singular_phone_number) == 5:
+    
+    # adds the non numerical 
+    if len(singular_phone_number) == 3:
       singular_phone_number += '-'
-    if len(singular_phone_number) == 9:
+    # if len(singular_phone_number) == 5:
+    #   singular_phone_number += '-'
+    if len(singular_phone_number) == 7:
       singular_phone_number += '-'
-    if len(singular_phone_number) == 14:
+    if len(singular_phone_number) == 12:
       phone_numbers_raw.append(singular_phone_number)
 print(phone_numbers_raw)
 
 no_dupe_phone = [*set(phone_numbers_raw)]
 
-print(len(no_dupe_phone))
+
+with open('../assets/phone_numbers.txt', 'w') as f:
+  for nums in no_dupe_phone:
+    f.write(nums + '\n')
